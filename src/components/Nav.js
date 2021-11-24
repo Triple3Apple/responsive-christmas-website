d13d6abeb06b18f545f0fcf6bb5d4d93135c57d8
Abrar Akhand <Jahin345@gmail.com>
2021-11-23 19:05:56 -0700

    finished mobile menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,44 +1,58 @@
-import React from 'react';
+import { useState } from 'react';
 import Logo from '../assets/img/logo.png';
 import NavImg from '../assets/img/gingerbread-man.png';
 
 
 const Nav = () => {
+
+    const [menuShown, setMenuVisibility] = useState(false);
+
+    const ToggleMenu = () => {
+        console.log('toggled menu');
+        setMenuVisibility(!menuShown);
+    }
+
+    const RemoveMenu = () => {
+        console.log('removed menu');
+        setMenuVisibility(false);
+    }
+
     return (
         <div className="nav container">
             <a href="#" className="nav-logo">
                 <img src={Logo} alt="" className="nav-logo-img" />
+                Christmas
             </a>
-            <div className="nav-menu" id="nav-menu">
+            <div className={`nav-menu ${(menuShown === true) ? 'show-menu' : ''}`} id="nav-menu">
                 <ul className="nav-list">
                     <li className="nav-item">
-                        <a href="#home" className="nav-link">Home</a>
+                        <a href="#home" className="nav-link" onClick={() => RemoveMenu()}>Home</a>
                     </li>
                     <li className="nav-item">
-                        <a href="#about" className="nav-link">About</a>
+                        <a href="#about" className="nav-link" onClick={() => RemoveMenu()}>About</a>
                     </li>
                     <li className="nav-item">
-                        <a href="#gifts" className="nav-link">Gifts</a>
+                        <a href="#gifts" className="nav-link" onClick={() => RemoveMenu()}>Gifts</a>
                     </li>
                     <li className="nav-item">
-                        <a href="#special-offer" className="nav-link">Special Offer</a>
+                        <a href="#special-offer" className="nav-link" onClick={() => RemoveMenu()}>Special Offer</a>
                     </li>
 
-                    <a href="#" className="button button-ghost">Support</a>
+                    <a href="#" className="button button-ghost" onClick={() => RemoveMenu()}>Support</a>
                 </ul>
 
 
                 <div className="nav-close" id="nav-close">
-                    <i class='bx bx-x'></i>
+                    <i className='bx bx-x' onClick={() => ToggleMenu()}></i>
                 </div>
 
                 <img src={NavImg} alt="" className="nav-img" />
             </div>
 
             <div className="nav-toggle" id="nav-toggle">
-                <i class='bx bx-grid-alt' ></i>
+                <i className='bx bx-grid-alt' onClick={() => ToggleMenu()}></i>
             </div>
-        </div>
+        </div >
     )
 }
 
